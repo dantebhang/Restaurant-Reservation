c460ef0a0f4596443405e9f6472a8def4e2c6f7d
bhangdante@gmail.com <87205105+dantebhang@users.noreply.github.com>
2021-11-27 12:07:33 -0800

    completed search page

--- front-end/src/reservations/ReservationsTable.js
+++ front-end/src/reservations/ReservationsTable.js
@@ -1,36 +1,39 @@
 import React from "react";
 import ReservationList from "./ReservationList";
 
-function ReservationTable({reservations}){
-
-    const reservationList = reservations.map((reservation) => (
+function ReservationTable({ reservations }) {
+	const reservationList = reservations.map((reservation) => (
 		<ReservationList
 			key={reservation.reservation_id}
 			reservation={reservation}
 		/>
 	));
-    
-    return(
-        <div>
-        <div className="table col-md-6">
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Name</th>
-                        <th>Phone</th>
-                        <th>Date</th>
-                        <th>Time</th>
-                        <th>People</th>
-                        <th>Status</th>
-                    </tr>
-                </thead>
-                {reservationList}
-            </table>
-        </div>
-    </div>
 
-    );
+	return (
+		<div>
+			<div className="table col-md-6">
+				<table className="table">
+					<thead>
+						<tr>
+							<th>#</th>
+							<th>Name</th>
+							<th>Phone</th>
+							<th>Date</th>
+							<th>Time</th>
+							<th>People</th>
+							<th>Status</th>
+						</tr>
+					</thead>
+				</table>
+				{reservations.length ? (
+					reservationList
+				) : (
+                    
+					<div>No reservations found</div>
+				)}
+			</div>
+		</div>
+	);
 }
 
 export default ReservationTable;
