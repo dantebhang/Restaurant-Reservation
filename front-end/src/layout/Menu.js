65a5c8a8646046142b43ca338928c4b09a365a38
bhangdante@gmail.com <87205105+dantebhang@users.noreply.github.com>
2021-12-01 19:12:38 -0800

    navbar margin

--- front-end/src/layout/Menu.js
+++ front-end/src/layout/Menu.js
@@ -11,34 +11,35 @@ function Menu() {
 
 
 	return (
-		<nav className="navbar navbar-expand navbar-light bg">
+		<nav className="navbar navbar-expand bg">
+			<div className="container">
 			<div
 				className="collapse navbar-collapse justify-content-center "
 				id="navbarNav"
 			>
-				<ul className="navbar-nav">
+				<ul className="nav navbar-nav">
 					<li className="mx-4 nav-item">
 						<NavLink exact to="/dashboard" activeClassName="active-link"  >
-							<p style={{ color: "white" }}>Dashboard</p>
+							<p className="m-2" style={{ color: "white" }}>Dashboard</p>
 						</NavLink>
 					</li>
 					<li className="mx-4 nav-item">
 						<NavLink to="/search" activeClassName="active-link" >
-							<p style={{ color: "white" }}>Search</p>
+							<p className="m-2" style={{ color: "white" }}>Search</p>
 						</NavLink>
 					</li>
 					<li className="mx-4 nav-item">
 						<NavLink to="/reservations/new" activeClassName="active-link" >
-							<p style={{ color: "white" }}>New Reservation</p>
+							<p className="m-2" style={{ color: "white" }}>New Reservation</p>
 						</NavLink>
 					</li>
 					<li className="mx-4 nav-item">
 						<NavLink to="/tables/new" activeClassName="active-link" >
-							<p style={{ color: "white" }}>New Table</p>
+							<p className="m-2" style={{ color: "white" }}>New Table</p>
 						</NavLink>
 					</li>
 				</ul>
-			</div>
+			</div></div>
 		</nav>
 	);
 }
